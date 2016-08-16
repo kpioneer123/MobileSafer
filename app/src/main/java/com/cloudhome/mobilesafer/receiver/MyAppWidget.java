8af32b9fea768f5ec96a69e036085d4870da3028
xionghu <965705418@qq.com>
2016-08-16 22:22:43 +0800

    定时杀死进程

---
+++ app/src/main/java/com/cloudhome/mobilesafer/receiver/MyAppWidget.java
@@ -0,0 +1,13 @@
+package com.cloudhome.mobilesafer.receiver;
+
+import android.appwidget.AppWidgetProvider;
+
+/**
+ * 特殊的广播接收者
+ * Created by xionhgu on 2016/8/16.
+ * Email：965705418@qq.com
+ */
+public class MyAppWidget extends AppWidgetProvider {
+
+
+}
